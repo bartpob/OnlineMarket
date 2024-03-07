f4bbb3101c0e495e458f709b003ecf8c37b9dc41
bartpob <bartlomiejpoblocki@gmail.com>
2024-03-07 01:03:21 +0100

    Added result abstraction for error handling

---
+++ OnlineMarket.Domain/Abstractions/Result/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Domain.Abstractions.Result
+{
+    public sealed record Error(string Code, string? Description = null)
+    {
+        public static readonly Error None = new(string.Empty);
+    }
+}
---
+++ OnlineMarket.Domain/Abstractions/Result/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Domain.Abstractions.Result
+{
+    public class Result
+    {
+        public bool IsSucceeded { get; private set; }
+        public bool IsFailure => !IsSucceeded;
+        public Error Error { get; private set; }
+        protected Result(bool isSucceeded, Error error)
+        {
+            if (isSucceeded && error != Error.None ||
+                !isSucceeded && error == Error.None)
+            {
+                throw new ArgumentException("Invalid error", nameof(Result));
+            }
+
+            IsSucceeded = isSucceeded;
+            Error = error;
+        }
+
+        public static Result Failure(Error error)
+        {
+            return new Result(false, error);
+        }
+
+        public static Result Succeeded()
+        {
+            return new Result(true, Error.None);
+        }
+    }
+
+    public sealed class Result<TBody>
+        : Result
+        where TBody : class
+    {
+
+        public TBody? Body { get; private set; }
+        private Result(bool isSucceeded, Error error, TBody? body)
+            : base(isSucceeded, error)
+        {
+            Body = body;
+        }
+
+        public new static Result<TBody> Failure(Error error)
+        {
+            return new Result<TBody>(false, error, null);
+        }
+
+        public static Result<TBody> Succeeded(TBody body)
+        {
+            return new(true, Error.None, body);
+        }
+    }
+}
