8bb255ab09ee6ee3c78dd4c2a3f9f1b80e4f65dc
bartpob <bartlomiejpoblocki@gmail.com>
2024-03-21 14:37:33 +0100

    Added GetWaitingAnnouncements command.

---
+++ OnlineMarket.Application/Announcements/GetWaitingAnnouncements/GetWaitingAnnouncementsCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using OnlineMarket.Application.Announcements.GetAllAnnouncements;
+using OnlineMarket.Domain.Abstractions.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Application.Announcements.GetWaitingAnnouncements
+{
+    public sealed record GetWaitingAnnouncementsCommand
+        : IRequest<Result<IEnumerable<AnnouncementResponse>>>;
+}
---
+++ OnlineMarket.Application/Announcements/GetWaitingAnnouncements/GetWaitingAnnouncementsCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using OnlineMarket.Application.Announcements.GetAllAnnouncements;
+using OnlineMarket.Domain.Abstractions.Result;
+using OnlineMarket.Domain.Announcements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarket.Application.Announcements.GetWaitingAnnouncements
+{
+    public sealed class GetWaitingAnnouncementsCommandHandler(IAnnoucementRepository _announcementRepository)
+        : IRequestHandler<GetWaitingAnnouncementsCommand, Result<IEnumerable<AnnouncementResponse>>>
+    {
+        public async Task<Result<IEnumerable<AnnouncementResponse>>> Handle(GetWaitingAnnouncementsCommand request, CancellationToken cancellationToken)
+        {
+            var announcements = await _announcementRepository.GetAllAsync();
+
+            return Result<IEnumerable<AnnouncementResponse>>.Succeeded(ToAnnouncementResponse(announcements.Where(a => a.Status == AnnouncementStatus.Waiting)));
+        }
+
+        private IEnumerable<AnnouncementResponse> ToAnnouncementResponse(IEnumerable<Announcement> announcements)
+        {
+            return announcements.Select(a => new AnnouncementResponse(
+                a.Id,
+                Guid.Parse(a.User.Id),
+                a.AnnouncementCategory.Name,
+                a.Description,
+                a.Price,
+                a.City,
+                a.Status,
+                a.Note
+                ));
+        }
+    }
+}
